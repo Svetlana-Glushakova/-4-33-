d1faf3bfc7658c10eb24a816cd14892c1fafef8b
Svetlana-Glushakova <55203250+Svetlana-Glushakova@users.noreply.github.com>
2019-10-09 21:13:02 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,42 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void perevod(int kolvo,int arr[])
+{
+	int i=0,k,x=0,W,fl,sum=0,count=0;
+	int *arr_d,*arr1;
+	arr_d=(int*)calloc(kolvo,sizeof(int));
+	for(i=0; i!=kolvo; i++)
+	{
+		arr1[i]=arr[i];
+	}
+	
+	for(i=0; i!=kolvo; i++)
+	{
+		while (arr[i]!=0)
+	    {
+		    k=arr[i]%10;
+		    arr[i]=arr[i]/10;
+		    W=1;
+		    fl=count;
+		    if(k==1)
+		    {
+			    if(fl==0)
+			    {
+				    sum++;
+			    }else 
+			    {
+				    while(fl>0)
+		            {
+			            W=W*2;
+			            fl--;
+		            }
+		            sum=sum+W;
+			    }
+		    }
+		    count++;
+	    }
+	    arr_d[x]=sum;
+	    x++;
+	    sum=0;
+	    count=0;
+	}
+	for (x=0; x!=kolvo; x++)
+	{
+		printf("Binary  %5d; Decimal  %3d\n", arr1[x], arr_d[x]);
+	}
+}
+
+
 
 int main()
 {
-	int n,W,k;
-	char str_1[100],*str_2;
+	char kolvo;
+	int i=0,n=0;
+	int *arr,*arr_d;
+	printf("Enter the number of numbers\n");
+	scanf("%d", &kolvo);
+	arr=(int*)calloc(kolvo,sizeof(int));
+	printf("\nEnter %d",kolvo);printf(" numbers\n");
+	while(i<kolvo)
+	{
+		scanf("%d", arr+i);
+		i++;
+	}
+	perevod(kolvo,arr);
 	
-		int x=0;
-		int count=0,sum=0;
-		printf("Enter:\n");
-	scanf("%s", str_1);
-	for(n=0; str_1[n]!='\0'; n++)
-	{
-		count++;
-	}
-	n=0;
-	while(str_1[n]<='1' && str_1[n]>='0' && str_1[n]!='\0')
-	{
-		str_2[x]=str_1[n];
-		x++;
-		k=count;
-		W=1;
-		if(str_1[n]=='1')
-		{
-			while(k!=1)
-		    {
-			    W=W*2;
-			    k--;
-		    }
-		    sum=sum+W;
-		}
-		count--;
-		n++;
-	}
-	if((str_1[n]<='1' && str_1[n]>='0') || str_1[n]=='\0')
-	{
-		printf("Binary number = %s  ", str_2);
-	    printf("\nDecimal number = %d\n", sum);
-	}else
-	printf("Error\n");
 	return 0;
 }
